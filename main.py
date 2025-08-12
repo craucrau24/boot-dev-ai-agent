ef3ebe0251a3285828a775fcfb03f5fd1ac4ed8a
Crawcraw <christophe.raucroy@gmail.com>
2025-08-12 11:33:37 +0200

    first ai request

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+resp = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(resp.text)
+
+print(f"""Prompt tokens: {resp.usage_metadata.prompt_token_count}
+Response tokens: {resp.usage_metadata.candidates_token_count}
+""")
+
