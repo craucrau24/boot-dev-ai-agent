b35c08836dcf3a8b2598e5da70f0dcb9f9d34ff6
Crawcraw <christophe.raucroy@gmail.com>
2025-08-12 11:40:55 +0200

    get prompt message from cli

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
+
+if len(sys.argv) < 2:
+  print("Missing prompt message")
+  sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-
+prompt = sys.argv[1]
 client = genai.Client(api_key=api_key)
-resp = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+resp = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 print(resp.text)
 
 print(f"""Prompt tokens: {resp.usage_metadata.prompt_token_count}
