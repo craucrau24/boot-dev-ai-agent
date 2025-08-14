988097d2292cfa1ae386378b1e76215f764da83a
Crawcraw <christophe.raucroy@gmail.com>
2025-08-14 20:28:32 +0200

    let ai-agent know which function can they call

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,7 @@
 import pathlib
 
+from google.genai import types
+
 from .utils import check_path_is_in_working_directory
 
 def get_files_info(working_directory, directory="."):
@@ -17,3 +19,18 @@ def get_files_info(working_directory, directory="."):
   dir_name = f"'{directory}'" if directory != "." else "current"
   header = f"Result for {dir_name} directory:\n"
   return header + "\n".join((f" - {chld.name}: file_size={stats.st_size} bytes, is_dir={chld.is_dir()}" for chld, stats in map(lambda child: (child, child.stat()), abs_path.iterdir())))
+
+
+schema_get_files_info = types.FunctionDeclaration(
+  name="get_files_info",
+  description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+  parameters=types.Schema(
+      type=types.Type.OBJECT,
+      properties={
+          "directory": types.Schema(
+              type=types.Type.STRING,
+              description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+          ),
+      },
+  ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 
+from functions.get_files_info import schema_get_files_info
 
 if len(sys.argv) < 2:
   print("Missing prompt message")
@@ -18,13 +19,39 @@ try:
 except IndexError:
     verbose = False
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 client = genai.Client(api_key=api_key)
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
-resp = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+resp = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+
 print(resp.text)
+function_calls = resp.function_calls if resp.function_calls is not None else []
+
+for function_call_part in function_calls:
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+
 
 if verbose:
     print(f"""User prompt: {user_prompt}
