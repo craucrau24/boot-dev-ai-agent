17dbcf543178b3ddfd42ef00275e18ab059793a0
Crawcraw <christophe.raucroy@gmail.com>
2025-08-14 11:15:03 +0200

    implement function for reading file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import pathlib
+
+def get_file_content(working_directory, file_path):
+  try:
+    abs_path = pathlib.Path(working_directory, file_path).resolve()
+  except:
+    return f"Error: couldn't resolve {file_path}"
+
+  if not str(abs_path).startswith(str(pathlib.Path(working_directory).resolve())):
+    return f'Error: Cannot list "{abs_path}" as it is outside the permitted working directory'
+
+  if not abs_path.is_file():
+    return f'Error: File not found or is not a regular file: "{file_path}"'
+
+  with open(abs_path, "r") as f:
+    try:
+      contents = f.read(10000)
+      if len(f.read(1)) != 0:
+        contents += f"[...File \"{file_path}\" truncated at 10000 characters]"
+    except IOError as e:
+        return f"Error: error reading file \"{file_path}\" - {e}"
+
+  return contents
--- tests.py
+++ tests.py
@@ -1,8 +1,17 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-for res in [get_files_info("calculator", "."),
-    get_files_info("calculator", "pkg"),
-    get_files_info("calculator", "/bin"),
-    get_files_info("calculator", "../")]:
+# operations = [get_files_info("calculator", "."),
+#     get_files_info("calculator", "pkg"),
+#     get_files_info("calculator", "/bin"),
+#     get_files_info("calculator", "../")]
+
+operations = [get_file_content("calculator", "main.py"),
+    get_file_content("calculator", "pkg/calculator.py"),
+    get_file_content("calculator", "pkg"),
+    get_file_content("calculator", "/bin/cat"),
+    get_file_content("calculator", "pkg/does_not_exist.py")]
+
+for res in operations:
   print(res)
   print()
